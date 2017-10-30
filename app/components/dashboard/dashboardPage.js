f373be5165a284c0791c6a982bf7a1be21c9c838
Tarun Sharma <Tarun.softengg@gmail.com>
2017-10-30 23:53:04 +0530

    chore funtionality

--- app/components/dashboard/dashboardPage.js
+++ app/components/dashboard/dashboardPage.js
@@ -17,7 +17,7 @@ const mapStateToProps = (state, ownProps) => {
   }
 }
 const mapDispatchToProps = dispatch => ({
- // fetchProfile: () => dispatch(Action.fetchProfile()),
+  // fetchProfile: () => dispatch(Action.fetchProfile()),
 });
 
 class DashBoardComponent extends React.Component {
@@ -28,37 +28,44 @@ class DashBoardComponent extends React.Component {
   componentWillMount() {
     //this.props.fetchProfile();
   }
-  logout() {
-
-  }
   render() {
     const {user} = this.props;
 
     return (
-    <div className="home--hero-header">
-    <div className="container">
-      <div className="row">
-        <div className="col-xs-12 text-center hero-header-container">
-          <div className="center-logo">
-            <a href="https://next.io">
-              <img src="https://s3.amazonaws.com/codementor_content/2016-Jun/code_fellows.png"/>
-            </a>
-          </div>
-          <h1 className="hero-header">Search Best programming tutorials</h1>
-          <div className="search-bar">
-            <div className="form-holder">
-              <div id="navbarSearch">
-                <input className="form-control navbar-search-input nav-input js-filter-topics" type="text" placeholder=" Search for the language you want to learn: Python, Javascript, ... "/>
+      <div>
+        <div className="home--hero-header">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 text-center hero-header-container">
+                <div className="center-logo">
+                  <a href="https://next.io">
+                    <img
+                      src="https://s3.amazonaws.com/codementor_content/2016-Jun/code_fellows.png"/>
+                  </a>
+                </div>
+                <h1 className="hero-header">Search Best programming tutorials</h1>
+                <div className="search-bar">
+                  <div className="form-holder">
+                    <div id="navbarSearch">
+                      <input
+                        className="form-control navbar-search-input nav-input js-filter-topics"
+                        type="text"
+                        placeholder=" Search for the language you want to learn: Python, Javascript, ... "/>
+                    </div>
+                    <i className="icon ion-ios-search-strong"></i>
+                  </div>
+                </div>
               </div>
-              <i className="icon ion-ios-search-strong"></i>
+              <div className="clearfix"></div>
             </div>
           </div>
-          <VideoList />
         </div>
-        <div className="clearfix"></div>
+        <div className="page--section">
+          <div className="container">
+            <VideoList/>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
     );
   }
 }
--- app/components/home/VideoList.js
+++ app/components/home/VideoList.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 
-class VideoList extends Component {
- render() {
+const VideoList = props => {
   return (
    <div className="row">
     <div className="col-xs-12 no-paddingright-desktop">
@@ -20,6 +19,30 @@ class VideoList extends Component {
          <p className="js-topic">React Development</p>
         </a>
        </div>
+       <div className="grid-col data-grid grid-col-1 topic-grid">
+        <a
+         href="https://next.io/tutorials/learn-android-development"
+         data-alias=""
+         className="topic-grid-item js-topic-grid-item">
+         <img
+          className="topic-thumbnail"
+          src="https://next.io/tutorials/learn-android-development/svg/android-development_logo"
+          alt=""/>
+         <p className="js-topic">Angular JS Development</p>
+        </a>
+       </div>
+       <div className="grid-col data-grid grid-col-1 topic-grid">
+        <a
+         href="https://next.io/tutorials/learn-android-development"
+         data-alias=""
+         className="topic-grid-item js-topic-grid-item">
+         <img
+          className="topic-thumbnail"
+          src="https://next.io/tutorials/learn-android-development/svg/android-development_logo"
+          alt=""/>
+         <p className="js-topic">Node Development</p>
+        </a>
+       </div>
       </div>
 
       <div id="no_topics_found" className="col-md-12 text-center hidden">
@@ -31,6 +54,5 @@ class VideoList extends Component {
    </div>
   );
  }
-}
 
 export default VideoList;
