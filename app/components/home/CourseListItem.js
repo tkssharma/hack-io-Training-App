c94ac3447119d8535d9299136e7ea251e86567d3
Tarun <Tarun.softengg@gmail.com>
2017-12-18 23:57:28 +0530

    updates

--- app/components/home/CourseListItem.js
+++ app/components/home/CourseListItem.js
@@ -7,6 +7,7 @@ const courseListItem = (props) => {
    <div className="topic-grid-item js-topic-grid-item">
     <a>
      <div>
+     <img className="topic-thumbnail" src={props.data.icon} alt=""/>
      <a href="" onClick={(e) => props.openCourse(props.data.name,e)}  rel="noopener noreferrer">{props.data.name}</a>
      </div>
     </a>
