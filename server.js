95a0292c947ca298530f5e6534d69c4bfd612ca9
Tarun <Tarun.softengg@gmail.com>
2017-12-19 23:35:10 +0530

    code updated to push on heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 const indexPath = path.join(__dirname, './index.html')
 const publicPath = express.static(path.join(__dirname, './public'))
 
-app.use('/public', publicPath)
+app.use('/', publicPath)
 
 app.get('/', function (req, res) { res.sendFile(indexPath) });
 const port = (process.env.PORT || 2244);
